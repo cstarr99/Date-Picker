20a1581dfd2a3d87262e0d00900ce7a5889c28cd
Chase Starr <chasestarr9@gmail.com>
2025-07-29 14:57:23 -0400

    made arrows change month in js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-import { format } from "date-fns";
+import {
+  format,
+  getUnixTime,
+  fromUnixTime,
+  addMonths,
+  subMonths,
+} from "date-fns";
+
+const datePickerButton = document.querySelector(".date-picker-button");
+const datePickerPopup = document.querySelector(".date-picker");
+const currentMonthLabel = document.querySelector(".current-month");
+const nextMonthBtn = document.querySelector(".next-month-button");
+const prevMonthBtn = document.querySelector(".prev-month-button");
+let currentDate = new Date();
+
+datePickerButton.addEventListener("click", () => {
+  datePickerPopup.classList.toggle("show");
+  const selectedDate = fromUnixTime(datePickerButton.dataset.dateDigits);
+  setupDatePicker(selectedDate);
+});
+
+nextMonthBtn.addEventListener("click", () => {
+  currentDate = addMonths(currentDate, 1);
+  setupDatePicker();
+});
+
+prevMonthBtn.addEventListener("click", () => {
+  currentDate = subMonths(currentDate, 1);
+  setupDatePicker();
+});
+
+function setDate(date) {
+  datePickerButton.innerText = format(date, "PPP");
+  datePickerButton.dataset.dateDigits = getUnixTime(date);
+}
+
+setDate(currentDate);
+
+function setupDatePicker() {
+  currentMonthLabel.innerText = format(currentDate, "LLLL - u");
+}
